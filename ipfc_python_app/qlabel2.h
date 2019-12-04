669ec8e7846716a901fef12cf44b984d7cc94dc6
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2019-12-05 00:19:04 +0800

    removed python

--- ipfc_python_app/qlabel2.h
+++
@@ -1,7 +0,0 @@
-class QLabel_changed(QLabel):
-    clicked=pyqtSignal()
-    def __init__(self, parent=None):
-        QLabel.__init__(self, parent)
-
-    def mousePressEvent(self, ev):
-        self.clicked.emit()
