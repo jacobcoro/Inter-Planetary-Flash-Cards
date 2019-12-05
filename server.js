0ba981ef0485daa6df14a83bbec834e0d012eba1
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2019-12-05 16:48:05 +0800

    heroku deploy

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+const serveStatic = require('serve-static');
+
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
+});
